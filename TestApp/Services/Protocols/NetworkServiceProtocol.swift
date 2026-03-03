9d046673c80718b50e8b744b4a8491ce80487971
Fantom <ivanneporadnyj@gmail.com>
2026-03-03 19:39:33 +0200

    Keep only List Screen files

--- TestApp/Services/Protocols/NetworkServiceProtocol.swift
+++
@@ -1,8 +0,0 @@
-protocol NetworkServiceProtocol {
-    var isConnected: Bool { get }
-    var connectionType: ConnectionType { get }
-    var connectivityStream: AsyncStream<Bool> { get }
-    
-    func startMonitoring()
-    func stopMonitoring()
-}
