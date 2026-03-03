b36f24fdde066f670c4f1f0b8168a2f0890deb32
Fantom <ivanneporadnyj@gmail.com>
2026-03-03 19:43:16 +0200

    Keep only Weather Service files

--- TestApp/Helpers/Date+Night.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension Date {
-    var isNightByCreationTime: Bool {
-        let hour = Calendar.current.component(.hour, from: self)
-        return hour >= 22 || hour < 6
-    }
-}
--- TestApp/Services/Protocols/StorageServiceProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SwiftData
-
-protocol StorageServiceProtocol {
-    func saveNote(text: String, timestamp: Date, weather: WeatherData?, localityName: String?) async throws
-    func delete<Object: PersistentModel>(_ object: Object) async throws
-    func fetch<Object: PersistentModel>(_ type: Object.Type, where predicate: Predicate<Object>) async throws -> [Object]
-    func fetchAll<Object: PersistentModel>(_ type: Object.Type) async throws -> [Object]
-}
